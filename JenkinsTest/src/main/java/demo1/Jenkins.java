0cd35e82fdec69c534bfc4c86c6f8513fc75bc8b
koyel mondal <koyel mondal@KMsLaptop0219>
2023-04-20 17:26:55 +0530

    Second commit.

--- JenkinsTest/src/main/java/demo1/Jenkins.java
+++ JenkinsTest/src/main/java/demo1/Jenkins.java
@@ -5,6 +5,7 @@ public class Jenkins {
 	public static void main(String[] args) {
 		System.out.println("Hello World... from Jenkins");
 		System.out.println("Hii Jenkins... from NETC");
+		System.out.println("lets work together");
 	}
 
 }
